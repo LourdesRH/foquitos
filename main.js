0b8c535df5a581cbc5b3888fecb2bc6cf3c6d4cd
LourdesRH <90708386+LourdesRH@users.noreply.github.com>
2022-03-17 10:27:35 -0600

    Update main.js

--- main.js
+++ main.js
@@ -21,110 +21,63 @@ recognition.addEventListener('result', (e)=>{
     .join('');
 
   p.innerText = text;
-
-
+  
   if(e.results[0].isFinal){
-
-
     if (text.includes('Prende uno') || text.includes('prende uno') || text.includes('prende 1')) {
-      p = document.createElement('p');
-      p.classList.add('replay');
-      p.innerText = 'Listo';
-      texts.appendChild(p)
       iduno.style.background = "url(bulb_on.jpg)";
     }
     
-    if (text.includes('Apaga uno') || text.includes('apaga uno') || text.includes('prende 1')) {
-      p = document.createElement('p');
-      p.classList.add('replay');
-      p.innerText = 'Listo';
-      texts.appendChild(p)
+    if (text.includes('Apaga uno') || text.includes('apaga uno') || text.includes('apaga 1')) {
       iduno.style.background = "url(bulb_off.jpg)";
     }
-    p = document.createElement('p');
+    
   }
 
   //foco dos
 
   if(e.results[0].isFinal){
     if (text.includes('Prende dos') || text.includes('prende dos') || text.includes('prende 2')) {
-      p = document.createElement('p');
-      p.classList.add('replay');
-      p.innerText = 'Listo';
-      texts.appendChild(p)
       iddos.style.background = "url(bulb_on.jpg)";
     }
     
-    if (text.includes('Apaga dos') || text.includes('apaga dos') || text.includes('prende 2')) {
-      p = document.createElement('p');
-      p.classList.add('replay');
-      p.innerText = 'Listo';
-      texts.appendChild(p)
+    if (text.includes('Apaga dos') || text.includes('apaga dos') || text.includes('apaga 2')) {
       iddos.style.background = "url(bulb_off.jpg)";
     }
-    p = document.createElement('p');
   }
 
   //foco tres
 
   if(e.results[0].isFinal){
     if (text.includes('Prende tres') || text.includes('prende tres') || text.includes('prende 3')) {
-      p = document.createElement('p');
-      p.classList.add('replay');
-      p.innerText = 'Listo';
-      texts.appendChild(p)
       idtres.style.background = "url(bulb_on.jpg)";
     }
     
     if (text.includes('Apaga tres') || text.includes('apaga tres') || text.includes('apaga 3')) {
-      p = document.createElement('p');
-      p.classList.add('replay');
-      p.innerText = 'Listo';
-      texts.appendChild(p)
       idtres.style.background = "url(bulb_off.jpg)";
     }
-    p = document.createElement('p');
   }
 
   //foco cuatro
   if(e.results[0].isFinal){
     if (text.includes('Prende cuatro') || text.includes('prende cuatro') || text.includes('prende 4')) {
-      p = document.createElement('p');
-      p.classList.add('replay');
-      p.innerText = 'Listo';
-      texts.appendChild(p)
       idcuatro.style.background = "url(bulb_on.jpg)";
     }
     
     if (text.includes('Apaga cuatro') || text.includes('apaga cuatro') || text.includes('apaga 4')) {
-      p = document.createElement('p');
-      p.classList.add('replay');
-      p.innerText = 'Listo';
-      texts.appendChild(p)
       idcuatro.style.background = "url(bulb_off.jpg)";
     }
-    p = document.createElement('p');
   }
 
   //foco cinco
 
   if(e.results[0].isFinal){
     if (text.includes('Prende cinco') || text.includes('prende cinco') || text.includes('prende 5')) {
-      p = document.createElement('p');
-      p.classList.add('replay');
-      p.innerText = 'Listo';
-      texts.appendChild(p)
       idcinco.style.background = "url(bulb_on.jpg)";
     }
     
     if (text.includes('Apaga cinco') || text.includes('apaga cinco') || text.includes('apaga 5')) {
-      p = document.createElement('p');
-      p.classList.add('replay');
-      p.innerText = 'Listo';
-      texts.appendChild(p)
       idcinco.style.background = "url(bulb_off.jpg)";
     }
-    p = document.createElement('p');
   }
 });
 
